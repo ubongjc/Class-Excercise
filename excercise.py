f420d6153d2a189ac730612d828831b6e5e810fe
Ubong Josiah <80695909+ubongjc@users.noreply.github.com>
2022-09-14 21:57:19 -0400

    Update excercise.py
    added third number

--- excercise.py
+++ excercise.py
@@ -1,7 +1,11 @@
 number_1 = int(input("Enter your first number: "))
 number_2 = int(input("Enter your second number: "))
-print(number_1 - number_2)
-print(number_1 + number_2)
-print(number_1 * number_2)
-print(number_1 ** number_2)
-print(number_1 / number_2)
+
+#Number from friend
+number_3 = int(input("Enter your third number: "))
+
+print(number_1 - number_2 - number_3)
+print(number_1 + number_2 + number_3)
+print(number_1 * number_2 * number_3)
+print(number_1 ** number_2 ** number_3)
+print(number_1 / number_2 / number_3)
